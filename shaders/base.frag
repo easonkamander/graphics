b9a62f4e25536fbec2911af54d1571dfef206ad3
Eason Kamander <eason@thinkpad495s.kamander.net>
2020-08-16 10:12:07 -0400

    Camera Pos + Dir

--- shaders/base.frag
+++ shaders/base.frag
@@ -1,7 +1,7 @@
 #version 460
-uniform sampler2D srcTex;
+uniform sampler2D tex;
 in vec2 point;
 out vec4 color;
 void main() {
-	color = texture(srcTex, point);
+	color = texture(tex, point);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
-#include <cstdlib>
 #include <cmath>
 
 GLuint initShader (GLenum type, std::string file_path) {
@@ -53,28 +51,19 @@ bool vldtProgram (GLuint program) {
 	return true;
 }
 
-int window_x, window_y;
-
-float cameraDir[3][3] = {
-	{1.0, 0.0, 0.0},
-	{0.0, 1.0, 0.0},
-	{0.0, 0.0, 1.0}
-};
-
-void setCameraDir (float a, float b, float c) {
-	cameraDir[0][0] = cosf(a)*cosf(c)+sinf(a)*sinf(b)*sinf(c);
-	cameraDir[1][0] = cosf(c)*sinf(a)*sinf(b) - cosf(a)*sinf(c);
-	cameraDir[2][0] = cosf(b)*sinf(a);
-	cameraDir[0][1] = cosf(b)*sinf(c);
-	cameraDir[1][1] = cosf(b)*cosf(c);
-	cameraDir[2][1] = -sinf(b);
-	cameraDir[0][2] = cosf(a)*sinf(b)*sinf(c) - cosf(c)*sinf(a);
-	cameraDir[1][2] = cosf(a)*cosf(c)*sinf(b) + sinf(a)*sinf(c);
-	cameraDir[2][2] = cosf(a)*cosf(b);
+void setCameraDirMat (float* cameraDir, float* cameraDirMat) {
+	cameraDirMat[0] = cosf(cameraDir[0])*cosf(cameraDir[2])+sinf(cameraDir[0])*sinf(cameraDir[1])*sinf(cameraDir[2]);
+	cameraDirMat[1] = cosf(cameraDir[2])*sinf(cameraDir[0])*sinf(cameraDir[1]) - cosf(cameraDir[0])*sinf(cameraDir[2]);
+	cameraDirMat[2] = cosf(cameraDir[1])*sinf(cameraDir[0]);
+	cameraDirMat[3] = cosf(cameraDir[1])*sinf(cameraDir[2]);
+	cameraDirMat[4] = cosf(cameraDir[1])*cosf(cameraDir[2]);
+	cameraDirMat[5] = -sinf(cameraDir[1]);
+	cameraDirMat[6] = cosf(cameraDir[0])*sinf(cameraDir[1])*sinf(cameraDir[2]) - cosf(cameraDir[2])*sinf(cameraDir[0]);
+	cameraDirMat[7] = cosf(cameraDir[0])*cosf(cameraDir[2])*sinf(cameraDir[1]) + sinf(cameraDir[0])*sinf(cameraDir[2]);
+	cameraDirMat[8] = cosf(cameraDir[0])*cosf(cameraDir[1]);
 }
 
-void resize (GLFWwindow* window, int width, int height) {
-	window_x = width, window_y = height;
+void resize (GLFWwindow* window, int window_x, int window_y) {
 	glViewport(0, 0, window_x, window_y);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, window_x, window_y, 0, GL_RGBA, GL_FLOAT, NULL);
 }
@@ -91,7 +80,7 @@ int main () {
 
 	GLFWwindow* window;
 
-	window_x = 1280, window_y = 720;
+	int window_x = 1280, window_y = 720;
 
 	if (!(window = glfwCreateWindow(window_x, window_y, "Graphics", 0, 0))) {
 		std::cerr << "Window Error" << std::endl;
@@ -122,7 +111,6 @@ int main () {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, window_x, window_y, 0, GL_RGBA, GL_FLOAT, NULL);
 	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8);
-	glGenerateMipmap(GL_TEXTURE_2D);
 
 	GLuint vertShader = initShader(GL_VERTEX_SHADER, "shaders/base.vert");
 	if(!vertShader) {
@@ -150,7 +138,7 @@ int main () {
 	}
 
 	glUseProgram(quadProgram);
-	glUniform1i(glGetUniformLocation(quadProgram, "srcTex"), 0);
+	glUniform1i(glGetUniformLocation(quadProgram, "tex"), 0);
 
 	GLuint quadVAO;
 	glGenVertexArrays(1, &quadVAO);
@@ -169,18 +157,18 @@ int main () {
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*8, quadPoints, GL_STREAM_DRAW);
 
-	GLint quadPtrPix = glGetAttribLocation(quadProgram, "pixel");
-	glVertexAttribPointer(quadPtrPix, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(quadPtrPix);
+	GLint quadPtrPixel = glGetAttribLocation(quadProgram, "pixel");
+	glVertexAttribPointer(quadPtrPixel, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(quadPtrPixel);
 
-	float savedLengthData [2*window_x*window_y] = {0.f};
+	// float savedLengthData [2*window_x*window_y] = {0.f};
 
-	GLuint ssboSavedLengths;
-	glGenBuffers(1, &ssboSavedLengths);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboSavedLengths);
-	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(savedLengthData), savedLengthData, GL_DYNAMIC_COPY);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssboSavedLengths);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+	// GLuint ssboSavedLengths;
+	// glGenBuffers(1, &ssboSavedLengths);
+	// glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboSavedLengths);
+	// glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(savedLengthData), savedLengthData, GL_DYNAMIC_COPY);
+	// glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssboSavedLengths);
+	// glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
 	GLuint compShader = initShader(GL_COMPUTE_SHADER, "shaders/main.comp");
 	if (!compShader) {
@@ -197,37 +185,28 @@ int main () {
 		glfwTerminate();
 		return 1;
 	}
-	
+
 	glUseProgram(compProgram);
 
-	GLint compPtrStage = glGetUniformLocation(compProgram, "stage");
-	GLuint compStageRst = 0;
-	GLuint compStageAdv = 1;
-	GLuint compStageClr = 2;
-	glUniform1ui(compPtrStage, compStageRst);
+	float cameraPos[3] = {0.f, 0.f, 0.f};
+	GLint ptrCameraPos = glGetUniformLocation(compProgram, "cameraPos");
+	glUniform3fv(ptrCameraPos, 1, &cameraPos[0]);
 
-	GLuint compMultiple[][2] = {
-		{144, 81},
-		{16, 9},
-		{4, 3},
-		{1, 1}
-	};
-	int compMultipleLen = sizeof(compMultiple)/sizeof(compMultiple[0]);
-	GLint compPtrMultiple = glGetUniformLocation(compProgram, "multiple");
-	glUniform2uiv(compPtrMultiple, 1, &compMultiple[0][0]);
+	float cameraDir[3] = {0.f, 0.f, 0.f};
+	float cameraDirMat[9];
+	GLint ptrCameraDir = glGetUniformLocation(compProgram, "cameraDir");
+	setCameraDirMat(&cameraDir[0], &cameraDirMat[0]);
+	glUniformMatrix3fv(ptrCameraDir, 1, GL_TRUE, &cameraDirMat[0]);
 
-	float compCameraPosVect[3] = {
-		0.f, 0.f, 0.f
-	};
-	GLint compPtrCameraPos = glGetUniformLocation(compProgram, "cameraPos");
-	glUniform3fv(compPtrCameraPos, 1, &compCameraPosVect[0]);
-
-	float compCameraDirVect[3] = {
-		0.f, 0.f, 0.f
-	};
-	GLint compPtrCameraDir = glGetUniformLocation(compProgram, "cameraDir");
-	setCameraDir(0.f, 0.f, 0.f);
-	glUniformMatrix3fv(compPtrCameraDir, 1, GL_FALSE, &cameraDir[0][0]);
+	// GLuint compMultiple[][2] = {
+	// 	{144, 81},
+	// 	{16, 9},
+	// 	{4, 3},
+	// 	{1, 1}
+	// };
+	// int compMultipleLen = sizeof(compMultiple)/sizeof(compMultiple[0]);
+	// GLint compPtrMultiple = glGetUniformLocation(compProgram, "multiple");
+	// glUniform2uiv(compPtrMultiple, 1, &compMultiple[0][0]);
 
 	double time = glfwGetTime();
 
@@ -235,14 +214,13 @@ int main () {
 		std::cout << glfwGetTime() - time << std::endl;
 		time = glfwGetTime();
 
-		for (int i = 0; i < compMultipleLen; i++) {
-			glUseProgram(compProgram);
-			glUniform1ui(compPtrStage, i == 0 ? compStageRst : (i + 1 == compMultipleLen ? compStageClr : compStageAdv));
-			glUniform2uiv(compPtrMultiple, 1, &compMultiple[i][0]);
-			glDispatchCompute((GLuint)(window_x - 1)/compMultiple[i][0] + 1, (GLuint)(window_y - 1)/compMultiple[i][1] + 1, 1);
-			glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-			glClear(GL_COLOR_BUFFER_BIT);
-		}
+		glUseProgram(compProgram);
+		glUniform3fv(ptrCameraPos, 1, &cameraPos[0]);
+		setCameraDirMat(&cameraDir[0], &cameraDirMat[0]);
+		glUniformMatrix3fv(ptrCameraDir, 1, GL_TRUE, &cameraDirMat[0]);
+		glDispatchCompute(window_x, window_y, 1);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(quadProgram);
 		glBindVertexArray(quadVAO);
@@ -254,28 +232,8 @@ int main () {
 		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
 			glfwSetWindowShouldClose(window, 1);
 		}
-		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_SPACE)) {
-			compCameraPosVect[2] += 0.03;
-		}
-		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_LEFT)) {
-			compCameraDirVect[0] -= 0.01;
-		}
-		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_RIGHT)) {
-			compCameraDirVect[0] += 0.01;
-		}
-		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_DOWN)) {
-			compCameraDirVect[1] += 0.01;
-		}
-		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_UP)) {
-			compCameraDirVect[1] -= 0.01;
-		}
 
 		glfwSwapBuffers(window);
-
-		glUseProgram(compProgram);
-		glUniform3fv(compPtrCameraPos, 1, &compCameraPosVect[0]);
-		setCameraDir(compCameraDirVect[0], compCameraDirVect[1], compCameraDirVect[2]);
-		glUniformMatrix3fv(compPtrCameraDir, 1, GL_FALSE, &cameraDir[0][0]);
 	}
 
 	glfwDestroyWindow(window);
